270dea9b7210ec2e78090e7106656e680b679548
zzingso <ravie90@gmail.com>
2021-03-24 20:07:46 +0900

    graphQL wip

---
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -0,0 +1,14 @@
+import { Field, InputType } from "@nestjs/graphql";
+
+
+@InputType()
+export class CreateRestaurantDto {
+    @Field(type => String)
+        name:string;
+    @Field(type => Boolean)
+        isVegan:boolean;
+    @Field(type => String)
+        address:string;
+    @Field(type => String)
+        ownerName:string;
+}
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,18 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
 
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver{
-    @Query(returns => RestaurantResolver)
-    myRestaurant() {
+    @Query(returns => [Restaurant])
+    restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[]{
+        return [];
+    }
+    @Mutation(returns => Boolean)
+    createRestaurant(
+        @Args('createRestaurantInput') createRestaurantInput: CreateRestaurantDto
+    ): boolean {
         return true;
     }
 }
